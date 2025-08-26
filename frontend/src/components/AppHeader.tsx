11dad6c62547f22744fb70237dc1f598b8c40d6e
seangah8 <seangah8@gmail.com>
2025-08-26 22:43:35 +0300

    feat: style app header

--- frontend/src/components/AppHeader.tsx
+++ frontend/src/components/AppHeader.tsx
@@ -7,7 +7,7 @@ export function AppHeader(){
 
     const navigation = useNavigate()
     const dispatch = useAppDispatch()
-    const shebaHeaderImage = "https://play-lh.googleusercontent.com/3eLZej2ZDq1EcneA28FOj4xfKvaoSqZ2XNcGwIz5U8vPNe2wgCeSkcfSmnUE-kQvmA"
+    const shebaHeaderImage = "https://shebaconnect.sheba.co.il/assets/images/sheba-logo-header-new.svg"
     const [isLogingOut, setIsLogingOut] = useState<boolean>(false)
 
     async function onLogout(){
@@ -18,9 +18,20 @@ export function AppHeader(){
 
     return(
         <section className='app-header'>
-            <div>
-                <button onClick={onLogout} disabled={isLogingOut}>Logout</button>
-                <button onClick={()=>navigation('/dashboard')} disabled={isLogingOut}>Home Page</button>
+            <div className='buttons-area'>
+
+                <button 
+                    className='logout' 
+                    onClick={onLogout} 
+                    disabled={isLogingOut}
+                >Logout</button>
+
+                <button 
+                    className='home-page' 
+                    onClick={()=>navigation('/dashboard')} 
+                    disabled={isLogingOut}
+                >Home Page</button>
+
             </div>
             <img src={shebaHeaderImage} alt='sheba-connect-image'/>
         </section>
