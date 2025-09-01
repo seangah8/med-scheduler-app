31c1ade0338a46d21fdc3ff391e7c4cf968be4cf
seangah8 <seangah8@gmail.com>
2025-09-02 02:00:35 +0300

    feat: enable toast notifications in app root

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import { useRoutes, useLocation, useNavigate } from 'react-router-dom'
 import { routes } from './router/routes'
 import { useAppSelector } from './store/hooks'
 import { AppHeader } from './components/AppHeader'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 
 function App() {
@@ -27,17 +29,30 @@ function App() {
 
   return (
 
-    <section className='app' style={!isUnderRegistration ? {width: '75%'} : undefined}> 
+    <>
+      <section
+        className='app'
+        style={!isUnderRegistration ? { width: '75%' } : undefined}
+      >
+        {/* show app header all the time but the registration page*/}
+        {!isUnderRegistration && <AppHeader />}
 
-      {/* show app header all the time but the registration page*/}
-      { 
-        !isUnderRegistration &&
-        <AppHeader/>
-      }
+        {routeElements}
+      </section>
 
-      {routeElements} 
-
-    </section>
+      <ToastContainer
+        position='top-left'
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='colored'
+      />
+    </>
   )
 }
 
