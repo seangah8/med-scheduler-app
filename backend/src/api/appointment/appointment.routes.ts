4a48b2713a32f107e96e988b7c6f750026eafc3f
seangah8 <seangah8@gmail.com>
2025-09-02 16:32:07 +0300

    refactor: check validateRequest before validateBooking in addAppointment route

--- backend/src/api/appointment/appointment.routes.ts
+++ backend/src/api/appointment/appointment.routes.ts
@@ -30,7 +30,7 @@ router.get('/', requireAuth, validateRequest(getAppointmentsSchema), getAppointm
 router.get('/:id', requireAuth, validateRequest(getAppointmentSchema), getAppointment)
 router.get('/unavailable-dates/:fieldId/:doctorId', requireAuth, validateRequest(getAllUnavailabilitySchema), getAllUnavailability)
 router.get('/pdf/:id', log, requireAuth, validateRequest(getAppointmentPdfSchema), getAppointmentPdf)
-router.post('/', log, requireAuth, validateBooking, validateRequest(addAppointmentSchema), addAppointment)
+router.post('/', log, requireAuth, validateRequest(addAppointmentSchema), validateBooking, addAppointment)
 router.patch('/cancel/:id', log, requireAuth, validateRequest(cancelAppointmentSchema), cancelAppointment)
 router.patch('/reschedule/:id/:date', log, requireAuth, validateRequest(rescheduleAppointmentSchema), rescheduleAppointment)
 router.patch('/virtual/:id/:isVirtual', log, requireAuth, validateRequest(changeAppointmentMethodSchema), changeAppointmentMethod)
